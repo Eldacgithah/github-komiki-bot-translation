bac48aefb633de712ed51af781c15cfdfe43a0b6
Eldacgithah <eeeldac@gmail.com>
2025-09-26 05:02:32 +0300

    Update arguments.py

--- app/arguments.py
+++ app/arguments.py
@@ -2,8 +2,8 @@ import argparse
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Process app configuration.")
+    parser = argparse.ArgumentParser(description="Обработка конфигурации приложения.")
     parser.add_argument(
-        "--config", "-c", type=str, help="configuration file", default="config.toml"
+        "--config", "-c", type=str, help="файл конфигурации", default="config.toml"
     )
     return parser.parse_args()
